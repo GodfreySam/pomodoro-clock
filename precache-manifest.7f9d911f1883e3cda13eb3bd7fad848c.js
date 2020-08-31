3d7f3f7c7c65eea6150e6dafee9782fd68340159
GodfreySam <godfreysam09@gmail.com>
2020-08-31 01:20:08 +0100

    Updates

---
+++ precache-manifest.7f9d911f1883e3cda13eb3bd7fad848c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2d7cb39c917b94be5248a20eacf1ba",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "97b16d949c4e56fc7f15",
+    "url": "/pomodoro-clock/static/css/main.e333c313.chunk.css"
+  },
+  {
+    "revision": "7e9f6a2d09c3984c8d66",
+    "url": "/pomodoro-clock/static/js/2.6703827a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.6703827a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b16d949c4e56fc7f15",
+    "url": "/pomodoro-clock/static/js/main.54b345a7.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
